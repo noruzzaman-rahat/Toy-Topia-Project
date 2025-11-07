7ed1c0c864579544d4ea7f67710769dcff29db6f
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-11-07 23:05:18 +0600

    desing login and register form

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -89,18 +89,18 @@ const SignIn = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-[#6d32f8] via-[#b832f5] to-[#e10080] px-4 py-12">
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-300 px-4 py-12">
       <title>Toy-Topia | Sign In</title>
       <form
         onSubmit={handleSignIn}
-        className="backdrop-blur-md bg-white/20 shadow-2xl border border-white/30 rounded-2xl px-10 py-8 w-full sm:w-3/4 md:w-2/3 lg:w-1/3 space-y-4"
+        className="backdrop-blur-md mt-[100px] bg-white shadow-2xl border border-white/30 rounded-2xl px-10 py-8 w-full sm:w-3/4 md:w-2/3 lg:w-1/3 space-y-4"
       >
-        <h2 className="text-3xl font-semibold text-center text-white mb-6">
+        <h2 className="text-3xl font-semibold text-center text-black mb-6">
           Sign In
         </h2>
 
         <div>
-          <label className="block mb-1 font-medium text-white">Email</label>
+          <label className="block mb-1 font-medium text-black">Email</label>
           <input
             type="email"
             name="email"
@@ -112,22 +112,22 @@ const SignIn = () => {
             }}
             value={email}
             placeholder="Enter your email"
-            className="w-full border border-purple-400/50 bg-white/10 text-white rounded-md px-4 py-2 outline-none focus:border-white focus:ring-1 focus:ring-white/80 placeholder:text-white/70"
+            className="w-full border border-gray-400/50 bg-white/10 text-black rounded-md px-4 py-2 outline-none focus:border-gray-300 focus:ring-1 focus:ring-white/80 placeholder:text-black/70"
           />
         </div>
 
         <div>
-          <label className="block mb-1 font-medium text-white">Password</label>
+          <label className="block mb-1 font-medium text-black">Password</label>
           <div className="relative">
             <input
               type={showsPassword ? "text" : "password"}
               name="password"
               placeholder="Enter your password"
               required
-              className="w-full border border-purple-400/50 bg-white/10 text-white rounded-md px-4 py-2 outline-none focus:border-white focus:ring-1 focus:ring-white/80 placeholder:text-white/70"
+              className="w-full border border-gray-400/50 bg-white/10 text-white rounded-md px-4 py-2 outline-none focus:border-white focus:ring-1 focus:ring-white/80 placeholder:text-black/70"
             />
             <span
-              className="absolute right-3 top-2.5 cursor-pointer text-white/80"
+              className="absolute right-3 top-2.5 cursor-pointer text-black/80"
               onClick={() => setShowPassword(!showsPassword)}
             >
               {showsPassword ? <IoEyeOutline /> : <FaRegEyeSlash />}
@@ -141,7 +141,7 @@ const SignIn = () => {
             onClick={() =>
               navigate("/forget", { state: { email: emailInput } })
             }
-            className="text-sm hover:underline text-white/80"
+            className="text-sm hover:underline text-black/80"
           >
             Forgot password?
           </button>
@@ -149,16 +149,16 @@ const SignIn = () => {
 
         <button
           type="submit"
-          className="w-full mt-3 py-3 bg-gradient-to-r cursor-pointer from-purple-600 to-pink-500  text-white font-semibold rounded-md shadow-md transition duration-200"
+          className="w-full mt-3 py-3 bg-gradient-to-r cursor-pointer from-red-600 to-green-500  text-white font-semibold rounded-md shadow-md transition duration-200"
         >
           Sign In
         </button>
 
-        <p className="text-center text-white/90 text-sm">
+        <p className="text-center text-gray/90 text-sm">
           Don’t have an account?{" "}
           <Link
             to="/register"
-            className="text-white hover:underline font-medium"
+            className="text-blue-500 hover:underline font-medium"
           >
             Sign Up
           </Link>
@@ -168,7 +168,7 @@ const SignIn = () => {
           <button
             type="button"
             onClick={handleGoogleLogin}
-            className="flex items-center gap-2 px-4 py-2 rounded-md bg-gradient-to-r from-red-500 to-red-600 text-white cursor-pointer hover:opacity-90 transition"
+            className="flex items-center gap-2 px-4 py-2 rounded-md bg-gray-200 text-black cursor-pointer hover:opacity-90 transition"
           >
             <FaGoogle /> Google
           </button>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -76,12 +76,12 @@ const Profile = () => {
   if (loading) return <Loading />;
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#6d32f8] to-[#e10080] pb-20 pt-30   px-4 ">
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-200 pb-20 pt-30   px-4 ">
       <title>Toy-Topia | My Profile</title>
 
       {/* Profile Info Card */}
       <div
-        className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 border border-purple-200 text-white p-8 rounded-xl shadow-lg text-center bg-gradient-to-r from-purple-600/40 to-pink-500/40 backdrop-blur-md"
+        className="w-full mt-[100px] md:w-3/4 lg:w-2/3 xl:w-1/2 border border-gray-200 text-black p-8 rounded-xl shadow-lg text-center bg-white backdrop-blur-md"
         data-aos="fade-up"
       >
         <img
@@ -94,14 +94,14 @@ const Profile = () => {
         <div className="mt-6 space-y-2" data-aos="fade-up" data-aos-delay="200">
           <h4 className="text-xl font-semibold">
             Name:{" "}
-            <span className="font-normal text-white/80">
+            <span className="font-normal text-black/80">
               {user?.displayName || "N/A"}
             </span>
           </h4>
-          <h4 className="text-white/80">
+          <h4 className="text-black/80">
             Email: <span>{user?.email || "N/A"}</span>
           </h4>
-          <h4 className="text-white/70 text-sm">
+          <h4 className="text-black/70 text-sm">
             Last Login:{" "}
             {user?.metadata?.lastSignInTime
               ? new Date(user.metadata.lastSignInTime).toLocaleString()
@@ -113,17 +113,17 @@ const Profile = () => {
       {/* Update Form */}
       <form
         onSubmit={handleUpdate}
-        className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 mt-10 bg-white/10 border border-purple-300/50 shadow-xl rounded-xl p-8 backdrop-blur-md"
+        className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 mt-[50px] bg-white border border-purple-300/50 shadow-xl rounded-xl p-8 backdrop-blur-md"
         data-aos="fade-up"
         data-aos-delay="400"
       >
-        <h2 className="text-center text-2xl font-semibold text-white mb-6">
+        <h2 className="text-center text-2xl font-semibold text-black mb-6">
           Update Your Information
         </h2>
 
         <div className="space-y-5">
           <div>
-            <label className="block text-white font-medium mb-2">
+            <label className="block text-black font-medium mb-2">
               Profile Photo URL
             </label>
             <input
@@ -131,12 +131,12 @@ const Profile = () => {
               name="photoURL"
               defaultValue={user?.photoURL}
               placeholder="Enter new photo URL"
-              className="w-full bg-transparent border border-purple-400 text-white placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-purple-500 outline-none transition-all"
+              className="w-full bg-transparent border border-gray-400 text-black placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-gray-500 outline-none transition-all"
             />
           </div>
 
           <div>
-            <label className="block text-white font-medium mb-2">
+            <label className="block text-black font-medium mb-2">
               Display Name
             </label>
             <input
@@ -144,25 +144,25 @@ const Profile = () => {
               name="name"
               defaultValue={user?.displayName}
               placeholder="Enter your name"
-              className="w-full bg-transparent border border-purple-400 text-white placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-purple-500 outline-none transition-all"
+              className="w-full bg-transparent border border-gray-400 text-black placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-gray-500 outline-none transition-all"
             />
           </div>
 
           <div>
-            <label className="block text-white font-medium mb-2">Email</label>
+            <label className="block text-black font-medium mb-2">Email</label>
             <input
               type="email"
               name="email"
               defaultValue={user?.email}
               placeholder="Enter your email"
-              className="w-full bg-transparent border border-purple-400 text-white placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-purple-500 outline-none transition-all"
+              className="w-full bg-transparent border border-gray-400 text-black placeholder-white/70 rounded-md py-2 px-4 focus:border-2 focus:border-gray-500 outline-none transition-all"
             />
           </div>
         </div>
 
         <button
           type="submit"
-          className="w-full mt-8 py-3 cursor-pointer bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow-md transition-all duration-200"
+          className="w-full mt-8 py-3 cursor-pointer bg-gradient-to-r cursor-pointer from-red-600 to-green-500  text-white font-semibold rounded-lg shadow-md transition-all duration-200"
           data-aos="zoom-in"
           data-aos-delay="600"
         >
